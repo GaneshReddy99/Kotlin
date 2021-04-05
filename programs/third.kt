db35935ec0dfa98a42ea4a71bf8332e2f92c4c56
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-04-05 10:59:58 +0530

    Create third.kt

---
+++ programs/third.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    var x=Integer.valueOf(readLine())
+    var y=Integer.valueOf(readLine())
+    var z=Integer.valueOf(readLine())
+    var t=x+y+z
+    var p=t/3
+    println("The total marks of student is $t")
+    println("The percentage of student is "+p+"%")
+}
