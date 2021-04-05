ebe753154e7ac8ca76de63175196492aa912882b
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-04-05 09:34:40 +0530

    Create Second.kt

---
+++ programs/Second.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    var sc= Scanner(System.`in`)
+    var r:Float=sc.nextFloat()
+    var p: Double =3.14
+    var radius:Double=p*r*r
+    println("The area of circle is: "+radius)
+    var perimeter:Double=2*p*r
+    println("The perimeter of circle is: "+ perimeter)
+    var lensqu:Double=perimeter/4
+    println("The length of side of square is: "+lensqu)
+}
